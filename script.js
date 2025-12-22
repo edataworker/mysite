a32a5279cf7003d952e2054a13a3b9fddfae0780
edataworker <edataworker@gmail.com>
2025-12-22 22:18:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,5 +44,38 @@ closeBtn.onclick = () => popup.style.display = "none";
 popup.onclick = e => {
   if (e.target === popup) popup.style.display = "none";
 }
+const blogContainer = document.getElementById("blogList");
+
+fetch("https://api.github.com/repos/edataworker/mysite/contents/blog")
+  .then(res => res.json())
+  .then(files => {
+    const posts = files
+      .filter(f => f.type === "file" && f.name.endsWith(".html"))
+      .sort((a, b) => new Date(b.name.split('-').slice(0,3).join('-')) - new Date(a.name.split('-').slice(0,3).join('-')));
+
+    posts.forEach(file => {
+      const name = file.name.replace(".html", "");
+      const parts = name.split("-");
+      const date = `${parts[2]}-${parts[1]}-${parts[0]}`;
+      const title = parts.slice(3).join(" ").replace(/-/g, " ");
+
+      const card = document.createElement("div");
+      card.className = "card";
+
+      card.innerHTML = `
+        <h3>${title}</h3>
+        <p><small>${date}</small></p>
+        <a class="cta" href="blog/${file.name}">Read More</a>
+      `;
+
+      blogContainer.appendChild(card);
+    });
+  })
+  .catch(() => {
+    blogContainer.innerHTML = "<p>Unable to load blog posts right now.</p>";
+  });
+
+
+
 
 
