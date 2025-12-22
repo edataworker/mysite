28641d2b27bbad89d2d5525749ad817999d6c082
edataworker <edataworker@gmail.com>
2025-12-23 00:33:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ fetch("https://api.github.com/repos/edataworker/mysite/contents/blog")
       card.innerHTML = `
         <h3>${title}</h3>
         <p><small>${date}</small></p>
-        <a class="cta" href="blog/${file.name}">Read More</a>
+        <a class="cta" href="blog/${file.name} target="_blank" ">Read More</a>
       `;
 
       blogContainer.appendChild(card);
@@ -74,6 +74,7 @@ fetch("https://api.github.com/repos/edataworker/mysite/contents/blog")
   .catch(() => {
     blogContainer.innerHTML = "<p>Unable to load blog posts right now.</p>";
   });
+
 
 
 
